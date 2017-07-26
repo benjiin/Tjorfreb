6239926e7221a7d5f7c1e0c5eb097be2693e99b5
benjaminr <benjaminr@e503-2-2.cbm.local>
2017-07-26 12:45:35 +0200

    Frank up to date

---
+++ Shop24_de/src/servlets/Warenkorb.java
@@ -0,0 +1,78 @@
+package servlets;
+
+import java.io.IOException;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+@WebServlet("/Warenkorb")
+public class Warenkorb extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	public Warenkorb()
+	{
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+	
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String resourcename = "java:comp/env/jdbc/dozent";
+		DataSource ds = null;
+
+		try
+		{
+			InitialContext jndiCntx = new InitialContext();
+			ds = (DataSource) jndiCntx.lookup(resourcename);
+			conn = ds.getConnection();
+			String SQL = "SELECT * FROM artikel";
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(SQL);						
+		}
+
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		finally
+		{
+			if (rs != null)
+				try
+				{
+					rs.close();
+				} catch (Exception e)
+				{
+				}
+			if (stmt != null)
+				try
+				{
+					stmt.close();
+				} catch (Exception e)
+				{
+				}
+			if (conn != null)
+				try
+				{
+					conn.close();
+				} catch (Exception e)
+				{
+				}
+		}
+
+	}
+	
+}
